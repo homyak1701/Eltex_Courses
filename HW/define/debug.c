87a6a6d0db2ca7b4e79f21353929caa4195e36a9
Andrey Dolgikh <DATA3214LOLLOl@yandex.ru>
2022-07-29 11:57:08 +0700

    макросыЖ

--- HW/define/debug.c
+++ HW/define/debug.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #define debug_print(fmt,...)\
-				do{if(DEBUG) printf(stderr, "%s:%d:%s()" fmt, __FILE__,}\
+				do{if(DEBUG) printf(stderr, "%s:%d:%s()" fmt, __FILE__,\
 					__LINE__, __func__, __VA_ARGS__);}while(0)
 
 
