5ac67b7e215e654278b9f75e1776d57c9cedd5c0
Andrey Dolgikh <DATA3214LOLLOl@yandex.ru>
2022-11-26 19:32:55 +0700

    26.11.2022 19.32

---
+++ Lectures/(-).библиотеки_в_си/libs_plugin/my_func.c
@@ -0,0 +1,11 @@
+int my_glob = 15;
+
+int my_func(int param)
+{
+        int ret;
+
+        ret = param + my_glob;
+
+        return ret;
+}
+
---
+++ Lectures/(-).библиотеки_в_си/libs_static/test.c
@@ -0,0 +1,13 @@
+#include "my_func.h"
+#include <stdio.h>
+
+
+int main(void)
+{
+	int a, c;
+
+	c = my_func(a);
+	printf("c = %d\n", c);
+
+	return 0;
+}
