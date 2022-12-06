30786673f1ba39857a234da64908b5b1815d8900
Andrey <DATA3214LOLLOl@yandex.ru>
2022-12-06 20:56:51 +0700

    06.12.2022 20.56

--- HW/2.Знакомство_с_СИ/№3/Zero.c
+++ HW/2.Знакомство_с_СИ/№3/Zero.c
@@ -5,22 +5,27 @@ int main()
 	int size = 0;
 	printf("Enter you size mas --- ");
 	scanf("%d", &size);
-	int k=2;
-
+	int x = 0;
+	int y = 0;
+	int z = 0;
+	//- ограничетель k, который будет помогать конец массива строки
+	//и каждый такт будет сдвигаться налево
+	int k=1;
 	int mas[size][size];
-	for (int x = 0; x < size; x++)
-	{
-		for(int y = 0; y < size; y++)
-		{
+
+	for (x = 0; x < size; x++){
+		//заполняем слева-напрво нулями до ограничителя
+		for(y = 0; y < size-k; y++){
 			mas[x][y] = 0;
 		}
-		for(int z = 1; z < k; z++)
-                {
+		//заполняем справа-налево единицами до ограничителя
+		for(z = 1; z < k+1; z++){
                 	mas[x][size-z] = 1;
                 }
 		k++;
 	}
 
+	// вывод заполненной матрицы
 	for (int x = 0;x < size; x++)
 	{
 		for(int y=0; y < size; y++)
