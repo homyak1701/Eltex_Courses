dc995e197ef17b34c505efcfa0242a09a137ab30
Andrey <DATA3214LOLLOl@yandex.ru>
2023-04-07 17:52:04 +0700

    07.04.2023 17:52

---
+++ HW/a12.Потоки/1/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define NUM_THREAD 10
+
+long a = 0;
+
+void *thread_start(void *arg){
+    int i;
+    int tmp;
+
+    for(i = 0; i < 1000000; i++){
+        tmp = a;
+        tmp++;
+        a = tmp;
+    }
+
+    printf("--> index = %d\n", *((int *)arg));
+
+    //pthread_exit(NULL);
+    return NULL;
+}
+
+int main(void){
+
+    pthread_t thread[NUM_THREAD];
+    int i;
+    int index[NUM_THREAD];
+    int index1;
+    int *s;
+
+    for(i=1; i<=NUM_THREAD; i++){
+        index[i-1] = i;
+        pthread_create(&thread[i-1], NULL, thread_start, &index[i-1]);
+    }
+
+    for(index1=0; index1 < NUM_THREAD; index1++){
+        pthread_join(thread[index1], (void **) &s);
+    }
+
+    printf("a = %ld\n", a);
+}
