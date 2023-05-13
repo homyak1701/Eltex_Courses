a2cebe94fb65d759098043fcb78d522aa91ff802
Andrey <DATA3214LOLLOl@yandex.ru>
2023-05-13 20:37:14 +0700

    13.05.2023 20:37

--- HW/a17.Высокопроизводительные серверы/3/client.c
+++ HW/a17.Высокопроизводительные серверы/3/client.c
@@ -7,17 +7,25 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/wait.h>
 
 #define error_func(a) do{if(-1 == a){ printf("line:%d\n", __LINE__); \
                                         perror("error"); exit(EXIT_FAILURE);}} while(0)
 
-#define MAIN_PORT 3452
+#define MAIN_PORT 3451
 
 int main(void){
 
     struct sockaddr_in serv;
+    struct sockaddr_in client;
+    //- тут храним размер для фукнции accept();
+    socklen_t len = sizeof(client);
     //- дескриптор сокета;
     int fd_serv;
+    int fd_request;
     //- проверка возвращаемых функций;
     int status;
     ssize_t amount_byte;
@@ -25,7 +33,7 @@ int main(void){
     int num;
     char num_char[10];
 
-    fd_serv = socket(AF_INET, SOCK_STREAM, 0);
+    fd_serv = socket(AF_INET, SOCK_DGRAM, 0);
         error_func(fd_serv);
 
     //-описывам наш сервер;
@@ -33,41 +41,32 @@ int main(void){
     serv.sin_port = htons(MAIN_PORT);
     serv.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    status = connect(fd_serv, (struct sockaddr *)&serv, sizeof(serv));
-        error_func(status);
-    
-    amount_byte = recv(fd_serv, (void *)&serv, sizeof(serv), 0);
-            error_func(amount_byte);
-
-    shutdown(fd_serv, SHUT_RDWR);
-    close(fd_serv);
-
-    fd_serv = socket(AF_INET, SOCK_STREAM, 0);
-        error_func(fd_serv);
-
-    printf("Я присоединяюсь по хосту %d\n", htons(serv.sin_port));
-    status = connect(fd_serv, (struct sockaddr *)&serv, sizeof(serv));
-        error_func(status);
+    printf("Отправляем запрос серверу\n");
 
     while(1){
 
-        printf("Напишем сереверу число: ");
+        printf("Напишем сереверу число (1 - минуты; 2 - секунды; 3 - часы): ");
         fgets(num_char, 5, stdin);
         num_char[strcspn(num_char, "\n")] = '\0';
         num = atoi(num_char);
 
 
-        amount_byte = send(fd_serv, (void *)&num, sizeof(num), 0);
+        amount_byte = sendto(fd_serv, (void *)&num, sizeof(num), 0, (struct sockaddr *)&serv, len);
             error_func(amount_byte);
 
-        amount_byte = recv(fd_serv, (void *)&num, sizeof(num), 0);
+        amount_byte = recvfrom(fd_serv, (void *)&num, sizeof(num), 0, NULL, NULL);
             error_func(amount_byte);
         
         if(0 == amount_byte){
             break;
+        } 
+
+        if(-1 == num){
+            printf("Такого запроса не существует\n");
+        } else {
+            printf("Сейчас: %d\n", num);
         }
-        
-        printf("Сервер разделил и получилось число: %d\n", num);
+
     }
 
     exit(EXIT_SUCCESS);
--- HW/a17.Высокопроизводительные серверы/3/server.c
+++ HW/a17.Высокопроизводительные серверы/3/server.c
@@ -12,49 +12,54 @@
 #include <sys/shm.h>
 #include <sys/sem.h>
 #include <sys/wait.h>
+#include <time.h>
 
 
 #define error_func(a) do{if(-1 == a){ printf("line:%d\n", __LINE__); \
                                         perror("error"); exit(EXIT_FAILURE);}} while(0)
 
 #define AMOUNT_MATH_SERV 10 
-#define NAME_FILE_KEY "key_sem"
-#define MAIN_PORT 3452
+#define NAME_FILE_KEY "key"
+#define MAIN_PORT 3451
 
 //- функция для потока, которая будет делить числа на два;
 void *math_server(void *arg);
 
 //- условные переменные для потоков, которые ожидают, что им дадут
 //поработать с клиентов;
-pthread_cond_t condition[AMOUNT_MATH_SERV] = {
-                                                PTHREAD_COND_INITIALIZER,
-                                                PTHREAD_COND_INITIALIZER,
-                                                PTHREAD_COND_INITIALIZER,
-                                                PTHREAD_COND_INITIALIZER,
-                                                PTHREAD_COND_INITIALIZER,
-                                                PTHREAD_COND_INITIALIZER,
-                                                PTHREAD_COND_INITIALIZER,
-                                                PTHREAD_COND_INITIALIZER,
-                                                PTHREAD_COND_INITIALIZER,
-                                                PTHREAD_COND_INITIALIZER
-};  
+pthread_cond_t condition[AMOUNT_MATH_SERV] = { PTHREAD_COND_INITIALIZER };  
 
-//- дескрипторы для клиентов;
-int fd_clients[AMOUNT_MATH_SERV] = { 0 };
+//- структура запроса;
+struct request{
+    int activity; //- если ли запрос или нет;
+    struct sockaddr_in client_info; //- сюда бдуем складывать информацию о клиенте;
+    int want; //- сюда будем складывать запрос пользователя;
+                //1 - это показать минуты;
+                //2 - это показать секунды;
+                //3 - показать часы;
+                //4 - показать все вместе;
+};
+
 //- количетсво потоков поддерживаемые сервером;
 pthread_t index_pthread[AMOUNT_MATH_SERV] = { 0 };
 //- дескрипторы для потоков, обрабатывающие клиентов;
 int fd_math_serv[AMOUNT_MATH_SERV] = { 0 };
-//- идентификатор еще не занятого сервера;
-int index_free_serv = 0;
 //- начальный порт;
-int port_serv[AMOUNT_MATH_SERV][2] = {{5689,0},{5690,0},{5691,},{5692,0},{5693,0},{5694,0},{5695,0},{5696,0},{5697,0},{5698,0}};
+int port_serv[AMOUNT_MATH_SERV] = {5689,5690,5691,5692,5693,5694,5695,5696,5697,5698};
+//- дескрипторы семафоров и разделяемой памяти;
+int fd_shmem;
+int fd_sem;
+//- идентификатор ключа System V
+key_t key;
+//- указатель на структуру запроса;
+struct request *request;
 
 //- мьютекст по обработки новых/выходящих клиентов
 pthread_mutex_t mutex_clients =  PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_queue[AMOUNT_MATH_SERV] = { PTHREAD_MUTEX_INITIALIZER };
 
 int main(void){
-
+    //- структура с информацией о клиенте и сервере;
     struct sockaddr_in serv;
     struct sockaddr_in client;
     //- дескриптор сокета;
@@ -66,31 +71,45 @@ int main(void){
     socklen_t len;
     //- индекс для циклов;
     int i = 0;
-    //- принимаем дескриптор клиента;
-    int invite_fd;
+    //- что за запрос нужен клиенту;
+    int wtf;
+
+    //- создаем ключ для разделяемой памяти и и семафоров;
+    key = ftok("key", 90);
+        error_func(key);
+    
+    //- создадим разделяемую память, куда бдуем помещать запросы от клиентов;
+    //- выделяем память под 10 запросов;
+    fd_shmem = shmget(key, sizeof(struct request) * 10, IPC_CREAT | 0666);
+        error_func(fd_shmem);
+    request = (struct request*)shmat(fd_shmem, NULL, 0);
+    memset(request, 0, sizeof(struct request) * 10);
+
+    //- создаем семафор;
+    fd_sem = semget(key, 1, IPC_CREAT | 0666);
+        error_func(fd_sem);
+    status = semctl(fd_sem, 0, SETVAL, 15);
+            error_func(status);
 
     //- создадим пул обслуживающих серверов;
     for(i = 0; i < AMOUNT_MATH_SERV; i++){
 
-        fd_math_serv[i] = socket(AF_INET, SOCK_STREAM, 0);
+        fd_math_serv[i] = socket(AF_INET, SOCK_DGRAM, 0);
             error_func(fd_math_serv[i]);
         
         serv.sin_family = AF_INET;
-        serv.sin_port = htons(port_serv[i][0]);
+        serv.sin_port = htons(port_serv[i]);
         serv.sin_addr.s_addr = inet_addr("127.0.0.1");
-
+        //- привязываем n-pointer 
         status = bind(fd_math_serv[i], (struct sockaddr *)&serv, sizeof(serv));
             error_func(status);
-
-        status = listen(fd_math_serv[i], 1);
-            error_func(status);
-
         //- потоку обязательно нужно передать дескриптор сокета обрабатывающего сервера;
         pthread_create(&index_pthread[i], NULL, math_server, (void *)&(fd_math_serv[i]));
+
     }
 
-    //- создаем сокет;
-    fd_sock = socket(AF_INET, SOCK_STREAM, 0);
+    //- создаем слушающий сокет;
+    fd_sock = socket(AF_INET, SOCK_DGRAM, 0);
         error_func(fd_sock);
 
     //-описывам наш сервер;
@@ -101,36 +120,37 @@ int main(void){
     status = bind(fd_sock, (struct sockaddr *)&serv, sizeof(serv));
         error_func(status);
     
-    printf("Начинаем прослушивать клиентов\n");
-
-    //- создаем очередь для подключения клиентов;
-    status = listen(fd_sock, 5);
-        error_func(status);
-    
     len = sizeof(client);
 
     while(1){
 
         sleep(1);
 
-        invite_fd = accept(fd_sock, (struct sockaddr *)&client, &len);
-            error_func(invite_fd);
+        printf("Ждем заявки от клиента\n");
+
+        //- получаем запрос от клиента;
+        amount_byte = recvfrom(fd_sock, (void *)&wtf, sizeof(wtf), 0, (struct sockaddr *)&client, &len);
+            error_func(amount_byte);
 
         pthread_mutex_lock(&mutex_clients);
 
-        //- ищем свободный обрабатывающий сервер;
+        //- ищем свободную заявку для обработки;
         for(i = 0; i < AMOUNT_MATH_SERV; i++){
-            if(0 == port_serv[i][1]){
-                port_serv[i][1] = 1;
-                break;
+            if(0 == (request + i)->activity){
+                status = pthread_mutex_trylock(&mutex_queue[i]);
+                if(0 == status){
+                    (request + i)->activity = 1;
+                    memcpy(&((request + i)->client_info), &client, sizeof(struct sockaddr));
+                    (request + i)->want = wtf;
+                    pthread_mutex_unlock(&mutex_queue[i]);
+                    break;
+                }
             }
         }
 
-        //- обновляем нужные дынные перед отправкой пользователю;
-        serv.sin_port = htons(port_serv[i][0]);
-        //-отправляем пользователю информацию об нужном потоке;
-        amount_byte = send(invite_fd, (void *)&serv, sizeof(serv), 0);
-            error_func(amount_byte);
+        //- говорим потокам, что появилась еще одна заявка;
+        status = semctl(fd_sem, 0, SETVAL, 0);
+            error_func(status);
 
         pthread_mutex_unlock(&mutex_clients);
 
@@ -148,6 +168,8 @@ int main(void){
 
 void *math_server(void *arg){
 
+    //- структура с информацией о клиенте;
+    struct sockaddr_in client;
     //- индекс обрабатывающего потока в системе;
     int fd_thread = *((int *)arg);
     //- то число, которое нужно будет разделить на два;
@@ -157,49 +179,78 @@ void *math_server(void *arg){
     int fd_client_math_serv;
     //- для ловки ошибок;
     int status;
-    //- для принятия дыннх от клиента
-    struct sockaddr_in client;
     //- тут храним размер для фукнции accept();
     socklen_t len = sizeof(client);
     //- принимаем дескриптор клиента;
     int invite_fd;
+    //- получим врямя, которое будем передавать клиентам;
+    time_t current_time;
+    struct tm* time_info;
+    //- что хочет клиент;
+    int wtf;
+
+
+    time(&current_time);
+    time_info = localtime(&current_time);
+
+    //- обрабатывающий сервер вхдит в очередь запросов, когда 
+    //появляется запрос и закрывает за собой дверь
+    struct sembuf go_to_queue[2] = {{0,0,0},{0,1,0}};
 
     while(1){
 
         sleep(1);
 
-        printf("Поток %d ждет клиента\n", fd_thread);
-        invite_fd = accept(fd_thread, (struct sockaddr *)&client, &len);
-            error_func(invite_fd);
-        printf("Поток %d дождался клиента\n", fd_thread);
+        semop(fd_sem, go_to_queue, 2);
 
-        while(1){
-            
-            sleep(1);
-
-            amount_byte = recv(invite_fd, (void *)&num, sizeof(num), 0);
-                error_func(amount_byte);
-
-            if(-1 == num){
-                break;
+        for(i = 0; i < AMOUNT_MATH_SERV; i++){
+            if(1 == (request + i)->activity){
+                //- чтобы потоки одновременно не забрали запросы сделаем trylock();
+                status = pthread_mutex_trylock(&mutex_queue[i]);
+                if(0 == status){
+                    printf("Поток %d вошел в разделяему память, чтобы забрать данные (%d)\n", fd_thread, (request + i)->want);
+                    memcpy(&client,&((request + i)->client_info), sizeof(struct sockaddr));
+                    wtf = (request + i)->want;
+                    (request + i)->activity = 0;
+                    pthread_mutex_unlock(&mutex_queue[i]);
+                    break;
+                }
             }
-                
-            printf("Клиент прислал цифру: %d\n", num);
-
-            num = num / 2;
-
-            amount_byte = send(invite_fd, (void *)&num, sizeof(num), 0);
-            error_func(amount_byte);
         }
         
-        pthread_mutex_lock(&mutex_clients);
+        printf("используем значение: %d\n", wtf);
 
-        close(invite_fd);
-        close(fd_thread);
+        client.sin_family = AF_INET;
+        client.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-        //- механизм высвобождения порта не был изобретен;
-        //... port_serv[fd_thread][1] = 0;
+        switch(wtf){
+            case 1:
+                wtf = time_info->tm_min;
+                amount_byte = sendto(fd_thread, (void *)&wtf, sizeof(wtf), 0, (struct sockaddr *)&client, len);
+                    error_func(amount_byte);
+                wtf = 0;
+                break;
+            case 2:
+                wtf = time_info->tm_sec; 
+                amount_byte = sendto(fd_thread, (void *)&wtf, sizeof(wtf), 0, (struct sockaddr *)&client, len);
+                    error_func(amount_byte);
+                    wtf = 0;
+                break;
 
-        pthread_mutex_unlock(&mutex_clients);
+            case 3:
+                wtf = time_info->tm_hour;
+                amount_byte = sendto(fd_thread, (void *)&wtf, sizeof(wtf), 0, (struct sockaddr *)&client, len);
+                    error_func(amount_byte);
+                wtf = 0;
+                break;
+
+            default:
+                wtf = -1;
+                amount_byte = sendto(fd_thread, (void *)&wtf, sizeof(wtf), 0, (struct sockaddr *)&client, len);
+                    error_func(amount_byte);
+        }
+
+        printf("Мы отправили %ld байт\n", amount_byte);
+
     }
 }
