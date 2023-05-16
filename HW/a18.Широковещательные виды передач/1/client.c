f580536f009be0cc5c2fdbd68b5c90899e8c040f
Andrey <DATA3214LOLLOl@yandex.ru>
2023-05-16 18:11:20 +0700

    16.05.2023 18:11

--- HW/a18.Широковещательные виды передач/1/client.c
+++ HW/a18.Широковещательные виды передач/1/client.c
@@ -20,18 +20,12 @@ int main(void){
     ssize_t amount_byte;
     //- буфер для записи;
     char sms[100];
-    //- для включения широковещательного адреса;
-    int flag_broatcast = 1;
 
     fd_client = socket(AF_INET, SOCK_DGRAM, 0);
 
     client.sin_family = AF_INET;
     client.sin_port = htons(7777);
     client.sin_addr.s_addr = htonl(INADDR_BROADCAST);
-
-    //- позволяем сокету делать широковещательную рассылку;
-    status = setsockopt(fd_client, SOL_SOCKET, SO_BROADCAST, &flag_broatcast, sizeof(flag_broatcast));
-        error_func(status);
 
     status = bind(fd_client, (struct sockaddr *)&client, sizeof(client));
         error_func(status);
